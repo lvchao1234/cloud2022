e8b811fd667c05b7cba4d07f924b878b50a826f5
lvchao <1023074978@qq.com>
2022-11-08 20:37:11 +0800

    1

--- cloud-provider-payment8001/src/main/java/com/lvchao/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/lvchao/springcloud/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.lvchao.springcloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
