fb1bf7f103fc90ec199e1d4c052d19ac70b2294e
lvchao <1023074978@qq.com>
2022-11-24 19:59:07 +0800

    1

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lvchao/springcloud/controller/SendMessageController.java
@@ -0,0 +1,21 @@
+package com.lvchao.springcloud.controller;
+
+import com.lvchao.springcloud.service.IMessageProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lvchao/springcloud/service/IMessageProvider.java
@@ -0,0 +1,6 @@
+package com.lvchao.springcloud.service;
+
+public interface IMessageProvider {
+
+    public String send();
+}
