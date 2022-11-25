da61cf574ebc5294b774e232979be144f38df68d
lvchao <1023074978@qq.com>
2022-11-25 18:02:28 +0800

    1

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/lvchao/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.lvchao.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+        log.info("PaymentMain9001启动成功了！");
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/lvchao/springcloud/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package com.lvchao.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+        log.info("PaymentMain9002启动成功了！");
+    }
+}
